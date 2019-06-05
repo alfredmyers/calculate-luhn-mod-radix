40fb421a5bc480f3b346c582b0af089b03db15f4
Alfred Myers <git@alfredmyers.com>
2019-06-05 22:58:26 +0000

    Updated dependencies

--- index.js
+++ index.js
@@ -3,9 +3,5 @@
 const calculateLuhnModN = require('calculate-luhn-mod-n');
 
 module.exports = function calculateLuhnModRadix(input, n){
-    var codePoints = new Array(input.length);
-    for (let index = 0; index < input.length; index++) {
-        codePoints[index] = parseInt(input[index], n);
-    }
-    return Number(calculateLuhnModN(codePoints, n)).toString(n);
+    return calculateLuhnModN(character => Number.parseInt(character, n), codePoint => codePoint.toString(n), n, input);
 }
