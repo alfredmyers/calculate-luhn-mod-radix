f0c2193763d2ca3dec2c1f81558ef68304185aeb
Alfred Myers <git@alfredmyers.com>
2019-06-05 22:31:16 +0000

    Added new tests

--- index.test.js
+++ index.test.js
@@ -6,6 +6,21 @@ const data = [
         input: '1134806PJFB000010013CD18',
         output: 'D'
     },
+    {
+        n: 36,
+        input: '1144701CEAA0000000004218',
+        output: 'S'
+    },
+    {
+        n: 36,
+        input: '1144701AU1087AE065175318',
+        output: 'P'
+    },
+    {
+        n: 36,
+        input: '111252331000000008229719',
+        output: 'H'
+    },
     //test based on algorithm description in https://en.wikipedia.org/wiki/Luhn_algorithm
     {
         n: 10,
